4078d8d8cfc923c09f5fbfda4055dfe89e3eddfa
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2011-09-17 16:16:36 -0300

    step 4. (broken commit) remove hardcoding of the table name

--- new_app/models.py
+++ new_app/models.py
@@ -4,7 +4,3 @@ from django.db import models
 class Something(models.Model):
     value = models.BooleanField(default = False)
     new_value = models.IntegerField(default = 0)
-
-    class Meta:
-        #for now leave table name as is
-        db_table = 'old_app_something'
